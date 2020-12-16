e48746e2c661e226ece9277c858312d96780c91d
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-16 18:36:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,6 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -16,4 +15,7 @@ class Scraper
     end
   end
 
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
 end
